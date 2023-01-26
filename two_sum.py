a3627672e99ab9e133573469b12f4187238c2613
BOOPESH-foxy <boopesh.mc@gmail.com>
2023-01-26 14:32:32 +0530

    two sum result

--- two_sum.py
+++ two_sum.py
@@ -2,6 +2,6 @@ values = []
 target = 4
 values = [2,1,5,3]
 for i in range(len(values)-1):
-    for j in (i+1,(len(values)-1)):
+    for j in range(i+1,(len(values))):
         if (values[i]+values[j] == target):
             print(i,j)
